5f2c59692aa77a514f4a8039cad416fa13773030
Sandroo <styopin.alexandr@gmail.com>
2022-10-08 19:17:29 +0300

    task - 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+console.dir(list);
+
+const createList = ingredients.map((ingredient) => {
+  const createLi = document.createElement('li');
+  createLi.className = "item";
+  createLi.textContent = ingredient;
+  return createLi;
+});
+
+
+
+list.append(...createList);
+
+console.log(createList);
