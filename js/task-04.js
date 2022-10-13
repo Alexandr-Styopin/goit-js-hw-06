9bbaf1b5182e2ec530443acc57a9da26d93b6069
Sandroo <styopin.alexandr@gmail.com>
2022-10-13 22:19:07 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,43 +1,55 @@
-const counter = {
-value: 0,
+// const counter = {
+// value: 0,
 
-decrement() {
-    this.value -=1;
-},
+// decrement() {
+//     this.value -=1;
+// },
 
-increment() {
-    this.value +=1;
-},
+// increment() {
+//     this.value +=1;
+// },
 
-};
+// };
 
+// const counterValue = document.querySelector('#value');
+// const decrementBtn = document.querySelector('button[data-action="decrement"]');
+// const incrementBtn = document.querySelector('button[data-action="increment"]');
 
-const counterValue = document.querySelector('#value');
+// decrementBtn.addEventListener('click', onDecrement);
+// incrementBtn.addEventListener('click', onIncrement);
+
+// function onDecrement (evt) {
+//     counter.decrement();
+//     counterValue.textContent = counter.value;
+// };
+
+// function onIncrement (evt) {
+//     counter.increment();
+//     counterValue.textContent = counter.value;
+// };
+
+// console.log(decrementBtn);
+// console.log(incrementBtn);
+
+//----------------------------------------------
+
+const valueEl = document.querySelector('#value');
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-
 
 decrementBtn.addEventListener('click', onDecrement);
 incrementBtn.addEventListener('click', onIncrement);
 
+let counterValue = 0;
+
 function onDecrement (evt) {
-
-    console.log('+');
-
-    counter.decrement();
-    counterValue.textContent = counter.value;
-
+    console.log('-');
+    valueEl.textContent = counterValue -=1;
 };
 
 function onIncrement (evt) {
-    console.log('-');
-
-
-    counter.increment();
-    counterValue.textContent = counter.value;
+    console.log('+');
+    valueEl.textContent = counterValue +=1;
 };
 
-
-
-console.log(decrementBtn);
-console.log(incrementBtn);
+console.log('counterValue =', counterValue);
