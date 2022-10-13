6ada8d87b43736afef92c5d629fa5b62b52b4081
Sandroo <styopin.alexandr@gmail.com>
2022-10-13 22:19:35 +0300

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,37 +1,3 @@
-// const counter = {
-// value: 0,
-
-// decrement() {
-//     this.value -=1;
-// },
-
-// increment() {
-//     this.value +=1;
-// },
-
-// };
-
-// const counterValue = document.querySelector('#value');
-// const decrementBtn = document.querySelector('button[data-action="decrement"]');
-// const incrementBtn = document.querySelector('button[data-action="increment"]');
-
-// decrementBtn.addEventListener('click', onDecrement);
-// incrementBtn.addEventListener('click', onIncrement);
-
-// function onDecrement (evt) {
-//     counter.decrement();
-//     counterValue.textContent = counter.value;
-// };
-
-// function onIncrement (evt) {
-//     counter.increment();
-//     counterValue.textContent = counter.value;
-// };
-
-// console.log(decrementBtn);
-// console.log(incrementBtn);
-
-//----------------------------------------------
 
 const valueEl = document.querySelector('#value');
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
@@ -51,5 +17,3 @@ function onIncrement (evt) {
     console.log('+');
     valueEl.textContent = counterValue +=1;
 };
-
-console.log('counterValue =', counterValue);
