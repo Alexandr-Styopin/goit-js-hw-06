1a00af44b38c9110e3cf5ed4388ec334297d4fee
Sandroo <styopin.alexandr@gmail.com>
2022-10-14 20:43:08 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,11 @@
 const fontSizeControl = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+
+fontSizeControl.addEventListener('input', onInput);
+
+let value = 0;
+
+function onInput(evt) {
+    value = fontSizeControl.value
+    text.style.fontSize = value + 'px';
+};
