1887861d071ad266199f3236775a0e81de25a7fb
Sandroo <styopin.alexandr@gmail.com>
2022-10-22 11:47:16 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,8 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-input.addEventListener('input', onInput);
+input.addEventListener('input', onInputText);
 
-function onInput(evt) {
+function onInputText(evt) {
     output.textContent = evt.currentTarget.value;
 };
