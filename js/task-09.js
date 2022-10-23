3777b6c1b9532f986bee9f4e646a8f3ef100d039
Sandroo <styopin.alexandr@gmail.com>
2022-10-23 17:15:28 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,7 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const bodyEl = document.querySelector('body');
 const colorEl = document.querySelector('.color')
 const changeColorBtn = document.querySelector('.change-color');
@@ -13,7 +12,3 @@ function onClickChangeColorBtn (evt) {
   bodyEl.style.backgroundColor = getRandomHexColor();
   colorEl.textContent = getRandomHexColor()
 };
-
-console.dir(bodyEl);
-console.dir(changeColorBtn);
-console.dir(colorEl);
