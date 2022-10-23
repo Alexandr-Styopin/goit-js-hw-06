8d5608b98702cea50a3560eb1b1afe159113e98c
Sandroo <styopin.alexandr@gmail.com>
2022-10-23 15:02:01 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+
+    evt.preventDefault();
+
+    const formElements = evt.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (email === "") {
+        alert('Поле email не заполненено или заполнено не верно');
+    } else if (password === "") {
+        alert('Поле password не заполненено или заполнено не верно');
+    }
+    else {
+        const formData = {
+            email, 
+            password,
+        };
+
+        console.log(formData);
+
+        form.reset();
+
+    };
+};
